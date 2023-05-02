3a523eaba4abc5c4cb65a187063375680daf9429
吴习章 <wuxz14@vanke.com>
2023-05-02 15:16:43 +0800

    feat: 明确单元测试的两个过程

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -20,7 +20,6 @@ export default {
             text: "为什么要写测试",
             link: "/guide/01-why-write-testing",
           },
-          { text: "自动化思维的体现", link: "/guide/02-automatic-thinking" },
           { text: "单元测试的定义", link: "/guide/03-unit-test-definition" },
           { text: "在什么时候写测试", link: "/guide/04-when-writing-tests" },
           { text: "不写测试的原因", link: "/guide/05-why-not-write-tests" },
